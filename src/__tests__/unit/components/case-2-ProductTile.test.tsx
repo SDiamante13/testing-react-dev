b9f20e41a062db7868293208b73866e4410ac4d1
Steven Diamante <SDiamante13@gmail.com>
2020-11-28 23:37:56 -0600

    Case 2: ProductTile tests added.

--- src/__tests__/unit/components/case-2-ProductTile.test.tsx
+++ src/__tests__/unit/components/case-2-ProductTile.test.tsx
@@ -1,18 +1,53 @@
 import React from 'react'
 import { axe } from 'jest-axe'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ProductTile from '../../../components/ProductTile'
 
 describe('The <ProductTile /> component', () => {
-  it('❌ renders a product tile with name, image and price', () => {
 
+  const defaultProductProps = {
+    id: 1,
+    image: 'shoes.png',
+    name: 'Shoes',
+    price: '42.00',
+    brand: 'Nike',
+    createdAt: '11/01/20',
+    isActive: true,
+    isNew: true,
+    isSoldOut: false,
+    priceUnformatted: 42.00
+  }
+
+  const setupProductTile = (props = defaultProductProps) => render(<ProductTile {...props}/>)
+
+  it('should match snapshot', () => {
+    const { asFragment } = setupProductTile();
+
+    expect(asFragment).toMatchSnapshot();
   })
 
-  it('❌ renders a product tile with name and price only', () => {
+  it('renders a product tile with name, image and price', () => {
+    setupProductTile();
 
+    screen.getByText(defaultProductProps.name);
+    screen.getByText(defaultProductProps.price);
+    screen.getByAltText(defaultProductProps.name);
   })
 
-  it('❌ has no accessibility violations', async () => {
+  it('renders a product tile with name and price only', () => {
+    setupProductTile({
+      ...defaultProductProps,
+      image: undefined
+    });
 
+    screen.getByText(defaultProductProps.name);
+    screen.getByText(defaultProductProps.price);
+    expect(screen.queryByTestId('ProductTileImage')).toBeNull();
+  })
+
+  it('has no accessibility violations', async () => {
+    const {container} = setupProductTile();
+
+    expect(await axe(container)).toHaveNoViolations();
   })
 })
