d276463eb67e3feec9242ee95805e826dc747566
Steven Diamante <SDiamante13@gmail.com>
2020-11-28 22:49:27 -0600

    Case 1: Checkbox tests added.

--- src/__tests__/unit/components/case-1-Checkbox.test.tsx
+++ src/__tests__/unit/components/case-1-Checkbox.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { axe } from 'jest-axe'
-import { render, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import Checkbox from '../../../components/Checkbox'
 
@@ -11,13 +11,52 @@ import Checkbox from '../../../components/Checkbox'
  * should.
  */
 describe('The <Checkbox /> component', () => {
-  it('❌ Should render the label and checkbox the user will see', () => {})
 
-  it('❌ Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {})
+  const defaultCheckboxProps = {
+    label: 'TEST_CHECKBOX_LABEL',
+    id: 'TEST_CHECKBOX_ID',
+    background: '#000',
+    checkMarkBackground: '#fff',
+    onChange: jest.fn(),
+    checked: false
+  }
 
-  it('❌ Should call the onChange handler when it is provided', () => {})
+  const setupCheckbox = (props = defaultCheckboxProps) => render(
+    <Checkbox {...props} />
+  );
 
-  it('❌ Should change state correctly when clicked (checked and unchecked)', () => {})
+  it('Should render the label and checkbox the user will see', () => {
+    const { asFragment } = setupCheckbox();
 
-  it('❌ should not fail any accessibility tests', async () => {})
+    expect(asFragment()).toMatchSnapshot();
+  })
+
+  it('Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {
+    setupCheckbox();
+
+    expect(screen.getByTestId("Checkbox")).toBeInTheDocument();
+  })
+
+  it('Should call the onChange handler when it is provided', () => {
+    setupCheckbox()
+
+    fireEvent.click(screen.getByLabelText(defaultCheckboxProps.label));
+
+    expect(defaultCheckboxProps.onChange).toHaveBeenCalled();
+  })
+
+  it('Should change state correctly when clicked (checked and unchecked)', () => {
+    setupCheckbox({
+      ...defaultCheckboxProps,
+      checked: true
+    })
+
+    expect(screen.getByLabelText(defaultCheckboxProps.label)).toBeChecked();
+  })
+
+  it('should not fail any accessibility tests', async () => {
+    const { container } = setupCheckbox();
+
+    expect(await axe(container)).toHaveNoViolations();
+  })
 })
